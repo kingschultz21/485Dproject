e7607e93b2a03d4212431815cb95293b81e801fc
Connor Schultz <connor.schultz@gmail.com>
2020-03-07 15:32:30 -0800

    Initial backend setup

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,9 @@ Basic routing program using Flask.
 import os, logging 
 
 # App Modules
+from app import app
+
+# Flask Modules
 from flask import render_template
 
 # Project Mapping Modules
@@ -18,29 +21,28 @@ import folium
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path>')
 def index(path):
-    # Link to Esri World Imagery service plus attribution
-    EsriImagery = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-    EsriAttribution = "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+	# Link to Esri World Imagery service plus attribution
+	EsriImagery = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+	EsriAttribution = "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 
-    cwd = os.getcwd()
+	cwd = os.getcwd()
 
-    # GOOGLE MAP
-    m = folium.Map(location=[53.5, -124], zoom_start=6.25, tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr = 'Google')
-    m.save('map.html')
-    # Sad fix to move html files from one directory to another as saving outside the cwd does
-    # not appear to be possible in folium
-    os.replace(cwd+'/map.html',cwd+'/app/templates/pages/map.html')
+	# GOOGLE MAP
+	m = folium.Map(location=[53.5, -124], zoom_start=6.25, tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',attr = 'Google')
+	m.save('map.html')
+	# Sad fix to move html files from one directory to another as saving outside the cwd does
+	# not appear to be possible in folium
+	os.replace(cwd+'/map.html',cwd+'/app/templates/pages/map.html')
+	print(cwd)
+	# ESRI MAP
+	m = folium.Map(location=[53.5, -124], zoom_start=6.25, tiles = EsriImagery,attr = EsriAttribution)
+	m.save('map2.html')
+	os.replace(cwd+'/map2.html',cwd+'/app/templates/pages/map2.html')
+	print(path)
+	try:
+		# try to match the pages defined in -> pages/<input file>
+		return render_template( 'pages/'+path )
 
-    # ESRI MAP
-    m = folium.Map(location=[53.5, -124], zoom_start=6.25, tiles = EsriImagery,attr = EsriAttribution)
-    m.save('map2.html')
-    os.replace(cwd+'/map2.html',cwd+'/app/templates/pages/map2.html')
-    
-    try:
-
-        # try to match the pages defined in -> pages/<input file>
-        return render_template( 'pages/'+path )
-    
-    except:
-        
-        return render_template( 'pages/error-404.html' )
+	except:
+		# return 404 error page
+		return render_template( 'pages/error-404.html' )
